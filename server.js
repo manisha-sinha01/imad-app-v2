8692bea23929519c03f5fce42f774f40cf8c1790
manisha-sinha01 <sinha.manisha01@gmail.com>
2017-02-09 00:04:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,15 +93,15 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(createTemplate(articletwo));
 });
 
 app.get('/article-three',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send(createTemplate(articlethree));
 });
 
 app.get('/ui/madi.png', function (req, res) {
