1d4e6b34e679a438e861f6ea8f7c4871fa5b0da4
manisha-sinha01 <sinha.manisha01@gmail.com>
2017-02-08 23:10:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var artcle-One={
+  title:Article One;  
+};
 var app = express();
 app.use(morgan('combined'));
 
